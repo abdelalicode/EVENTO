2dbd62e5af3e1b3bb450f8a6ab6f77817f827c61
abdelalicode <abdeltahaali@gmail.com>
2024-03-09 16:09:09 +0100

    Tache d'Organisateur
    Refactorisation du code

--- resources/views/Organisator/events.blade.php
+++ resources/views/Organisator/events.blade.php
@@ -329,7 +329,7 @@
 
 
                                         <!-- Modal toggle -->
-                                        <button data-modal-target="modal{{$event->id}}" data-modal-toggle="modal{{$event->id}}"
+                                        <button data-modal-target="modal{{$event->title}}" data-modal-toggle="modal{{$event->title}}"
                                             class="block text-white bg-transparent  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                             type="button">
                                             <img width="24" height="24"
@@ -338,7 +338,7 @@
                                         </button>
 
                                         <!-- Main modal -->
-                                        <div id="modal{{$event->id}}" tabindex="-1" aria-hidden="true"
+                                        <div id="modal{{$event->title}}" tabindex="-1" aria-hidden="true"
                                             class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
                                             <div class="relative p-4 w-full max-w-2xl max-h-full">
                                                 <!-- Modal content -->
@@ -352,7 +352,7 @@
                                                         </h3>
                                                         <button type="button"
                                                             class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                                            data-modal-hide="modal{{$event->id}}">
+                                                            data-modal-hide="modal{{$event->title}}">
                                                             <svg class="w-3 h-3" aria-hidden="true"
                                                                 xmlns="http://www.w3.org/2000/svg" fill="none"
                                                                 viewBox="0 0 14 14">
